1c23224265fc735ad03204781148baed4447d706
wwalpha <wwalpha@gmail.com>
2023-07-21 04:26:54 +0000

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count1 is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
