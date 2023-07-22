2ec703f09cecef4dbaa8d562777f05a6eb2d1dc9
wwalpha <wwalpha@gmail.com>
2023-07-22 06:20:19 +0000

    update

--- terraform/computing/search.tf
+++ terraform/computing/search.tf
@@ -1,11 +1,11 @@
 # ----------------------------------------------------------------------------------------------
 # Azure Cognitive Search
 # ----------------------------------------------------------------------------------------------
-resource "azurerm_search_service" "this" {
-  name                = "search${var.suffix}"
-  resource_group_name = var.resource_group_name
-  location            = var.resource_group_location
-  sku                 = "basic"
-  replica_count       = 1
-  partition_count     = 1
-}
+# resource "azurerm_search_service" "this" {
+#   name                = "search${var.suffix}"
+#   resource_group_name = var.resource_group_name
+#   location            = var.resource_group_location
+#   sku                 = "basic"
+#   replica_count       = 1
+#   partition_count     = 1
+# }
